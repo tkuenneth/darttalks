860612bc1fa2a5395126cc441c0dd68915e3bfa3
Thomas Künneth <thomas@thomaskuenneth.com>
2017-01-10 15:05:21 +0100

    Switched to a more Dart-like comments style

--- TemperatureConverter/lib/converter.dart
+++ TemperatureConverter/lib/converter.dart
@@ -1,23 +1,7 @@
-/*
- * converter.dart
- *
- * Copyright 2017 Thomas Kuenneth
- *
- * This file is part of TemperatureConverter.
- *
- * TemperatureConverter is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+// Copyright (c) 2017, Thomas Kuenneth.
+// All rights reserved. Use of this source code is governed by the
+// GNU General Public license version 3 that can be found in the LICENSE file.
+
 library converter;
 
 const String DEGREES_CELSIUS = "\u00B0C";
