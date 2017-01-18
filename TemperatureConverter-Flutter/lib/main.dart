c7712c89b3037a3fc4346823f4dbc3bb79ed8625
Thomas Künneth <thomas@thomaskuenneth.com>
2017-01-18 13:27:48 +0100

    Added padding

--- TemperatureConverter-Flutter/lib/main.dart
+++ TemperatureConverter-Flutter/lib/main.dart
@@ -3,6 +3,10 @@ import 'converter.dart';
 import 'model.dart';
 
 const _appName = 'Temperature Converter';
+const _temperature = 'Temperature';
+const EdgeInsets edgeInsetsLeftOnly =
+    const EdgeInsets.fromLTRB(18.0, 0.0, 0.0, 0.0);
+const _calculate = 'Calculate';
 
 void main() {
   runApp(new MyApp());
@@ -13,9 +17,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: _appName,
-      theme: new ThemeData(
-        primarySwatch: Colors.blue,
-      ),
       home: new MyHomePage(title: _appName),
     );
   }
@@ -40,45 +41,50 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: new AppBar(
         title: new Text(config.title),
       ),
-      body: new Column(children: <Widget>[
-        new Row(children: <Widget>[
-          new Expanded(
-              child: new Input(
-                  hintText: 'Temperature',
-                  value: _currentInput,
-                  onChanged: (input) => _currentInput = input)),
-          new DropdownButton(
-              items: createUnit(),
-              onChanged: (temperatureUnit newValue) {
-                setState(() {
-                  model.inUnit = newValue;
-                });
-              },
-              value: model.inUnit)
-        ]),
-        new Row(children: <Widget>[
-          new Text("Convert to"),
-          new DropdownButton(
-              items: createUnit(),
-              onChanged: (temperatureUnit newValue) {
-                setState(() {
-                  model.outUnit = newValue;
-                });
-              },
-              value: model.outUnit),
-        ]),
-        new FlatButton(
-            child: new Text('Calculate'),
-            onPressed: () {
-              setState(() {
-                double inTemp = stringToDouble(_currentInput.text);
-                model.inTemperature = inTemp;
-                model.calculateOutTemperature();
-              });
-            }),
-        new Text(model.outTemperatureAsString)
-      ]), // This trailing comma tells the Dart formatter to use
-      // a style that looks nicer for build methods.
+      body: new Block(
+          padding: new EdgeInsets.symmetric(vertical: 8.0, horizontal: 8.0),
+          children: <Widget>[
+            new Row(children: <Widget>[
+              new Expanded(
+                  child: new Input(
+                      labelText: _temperature,
+                      value: _currentInput,
+                      onChanged: (input) => _currentInput = input)),
+              new Container(
+                  padding: edgeInsetsLeftOnly,
+                  child: new DropdownButton(
+                      items: createUnit(),
+                      onChanged: (temperatureUnit newValue) {
+                        setState(() {
+                          model.inUnit = newValue;
+                        });
+                      },
+                      value: model.inUnit))
+            ]),
+            new Row(children: <Widget>[
+              new Text("Convert to"),
+              new Container(
+                  padding: edgeInsetsLeftOnly,
+                  child: new DropdownButton(
+                      items: createUnit(),
+                      onChanged: (temperatureUnit newValue) {
+                        setState(() {
+                          model.outUnit = newValue;
+                        });
+                      },
+                      value: model.outUnit)),
+            ]),
+            new FlatButton(
+                child: new Text(_calculate),
+                onPressed: () {
+                  setState(() {
+                    double inTemp = stringToDouble(_currentInput.text);
+                    model.inTemperature = inTemp;
+                    model.calculateOutTemperature();
+                  });
+                }),
+            new Text(model.outTemperatureAsString)
+          ]),
     );
   }
 
