a3f3547953e60eb693e434eea4773150e78a398f
Thomas Kuenneth <kuennetht@gmail.com>
2019-05-01 13:30:09 +0200

    switch-case demo added

---
+++ slides_samples/switch-case.dart
@@ -0,0 +1,17 @@
+main() {
+  switch (getANumber()) {
+    case 1:
+      print("eins");
+      break;
+    case 3:
+      print("drei");
+      break;
+    case 2:
+      print("zwei");
+      break;
+  }
+}
+
+int getANumber() {
+  return 3;
+}
